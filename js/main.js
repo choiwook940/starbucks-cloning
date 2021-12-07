9bd8396284439519d20597d0fd67e4876a0626cf
Kimham <choiwook940@naver.com>
2021-12-07 23:52:32 +0900

    feat: Add badges.
    resolve #2

--- js/main.js
+++ js/main.js
@@ -14,3 +14,18 @@ searchInput.addEventListener("blur", () => {
   search.classList.remove("focused");
   searchInput.setAttribute("placeholder", " ");
 })
+
+const badges = document.querySelector("header .badges");
+window.addEventListener("scroll", _.throttle(() => {
+  if(window.scrollY > 500){
+    gsap.to(badges, 0.6, {
+      opacity: 0,
+      display: "none"
+    })
+  } else {
+    gsap.to(badges, 0.6, {
+      opacity: 1,
+      display: "block"
+    })
+  }
+}, 300))
